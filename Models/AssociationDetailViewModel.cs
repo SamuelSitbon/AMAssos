66f0640f43494fe5b64262324c1f2e24df867679
Samuel Sitbon <samuel.sitbon.paroissien@gmail.com>
2020-03-10 17:51:42 +0100

    Création de la page Sorties par association -> Controller Associations -> Models -> AssociationDetailViewModel avec dictionnaire de sorties

---
+++ Models/AssociationDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMAssos.Models
+{
+    public class AssociationDetailViewModel
+    {
+        public Association Association { get; set; }
+        public Dictionary<string, List<Sortie>> Sorties { get; set; }
+    }
+}
